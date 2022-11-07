3d68e8a5d6b221e8fbf375de7b5163603563793d
Adegbuyi Oluwakayode <kayode_adegbuyi@yahoo.com>
2022-11-07 08:17:45 +0100

    Added Resources

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'isbn' => $this->isbn,
+            'authors' => $this->authors,
+            'number_of_pages' => $this->number_of_pages,
+            'publisher' => $this->publisher,
+            'country' => $this->country,
+            'release_date' => Carbon::parse($this->released)->format('Y-m-d')
+        ];
+    }
+}
